cbc5c1a7d1461e0be24ccbcc0fc038ce0ca204e4
qtopie.rw <qtopie.rw@outlook.com>
2021-10-21 00:43:01 +0800

    complete image paste

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.16
 
 require (
 	github.com/govim/govim v0.0.34
+	golang.design/x/clipboard v0.5.3
 	gopkg.in/tomb.v2 v2.0.0-20161208151619-d5d1b5820637
 )
--- image.go
+++ image.go
@@ -1,14 +1,54 @@
 package main
 
-import "github.com/govim/govim"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/govim/govim"
+	"golang.design/x/clipboard"
+)
 
 func pasteImage(g govim.Govim, flags govim.CommandFlags, args ...string) error {
 	rawMessage, err := g.ChannelExpr("expand('%:p')")
 	if err != nil {
-		g.Logf("%v", err)
+		return err
 	}
-	g.Logf("Messages %s", rawMessage)
-	return nil
+
+	if rawMessage == nil || len(rawMessage) == 0 {
+		// create file first
+			g.ChannelEx("echomsg " + "'create file first, then paste image again'")
+		return nil
+	}
+
+	mdFilePath := strings.Trim(string(rawMessage), `"`)
+	g.Logf("Messages %s", mdFilePath)
+
+	b := clipboard.Read(clipboard.FmtImage)
+	if b == nil {
+		g.ChannelEx("echomsg " + "'no image in system clipboard'")
+		return nil
+	}
+
+	assertsDir := strings.ToLower(strings.TrimSuffix(mdFilePath, filepath.Ext(mdFilePath))) + ".assets"
+	err = os.MkdirAll(assertsDir, os.ModePerm)
+	if err != nil {
+		return err
+	}
+
+	g.Logf(assertsDir)
+	pictureName := generatePictureFileName()
+	file := filepath.Join(assertsDir, pictureName)
+	return os.WriteFile(file, b, os.ModePerm)
+}
+
+func generatePictureFileName() string {
+	t := time.Now()
+
+	return fmt.Sprintf("img-%d%02d%02d%02d%02d%02d.png", t.Year(), t.Month(),
+		t.Day(), t.Hour(), t.Minute(), t.Second())
 }
 
 func cleanImage(g govim.Govim, flags govim.CommandFlags, args ...string) error {
