47438a18af9cb2f346746a29d8b37fca3b5cda3c
qtopie.rw <qtopie.rw@outlook.com>
2021-10-25 16:21:01 +0800

    complete preview draft codes

--- go.mod
+++ go.mod
@@ -4,7 +4,17 @@ go 1.16
 
 require (
 	github.com/chai2010/webp v1.1.0
+	github.com/google/go-cmp v0.5.6 // indirect
 	github.com/govim/govim v0.0.34
+	github.com/kr/pretty v0.3.0 // indirect
+	github.com/rogpeppe/go-internal v1.8.0 // indirect
+	github.com/stretchr/testify v1.7.1-0.20210427113832-6241f9ab9942 // indirect
+	github.com/yuin/goldmark v1.4.0
 	golang.design/x/clipboard v0.5.3
+	golang.org/x/exp v0.0.0-20200224162631-6cc2880d07d6 // indirect
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	golang.org/x/tools v0.1.5 // indirect
 	gopkg.in/tomb.v2 v2.0.0-20161208151619-d5d1b5820637
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- preview.go
+++ preview.go
@@ -1,8 +1,76 @@
 package main
 
-import "github.com/govim/govim"
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"os/exec"
+	"runtime"
+	"strings"
+
+	"github.com/govim/govim"
+	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark/extension"
+	"github.com/yuin/goldmark/parser"
+	"github.com/yuin/goldmark/renderer/html"
+)
 
 func previewMarkdown(g govim.Govim, flags govim.CommandFlags, args ...string) error {
-	g.Logf("command preview is called")
+	rawMessage, err := g.ChannelExpr("join(getline(1, '$'), '\n')")
+	if err != nil {
+		return err
+	}
+
+	content := strings.Trim(string(rawMessage), `"`)
+	content = strings.Replace(content, `\n`, "\n", -1)
+	g.Logf(content)
+	go serveHttp(content)
 	return nil
+}
+
+func serveHttp(content string) {
+	buf := renderMarkdown(content)
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		rw.Write(buf.Bytes())
+	})
+
+	openBrowser("http://127.0.0.1:7070")
+	http.ListenAndServe("127.0.0.1:7070", nil)
+}
+
+func renderMarkdown(content string) bytes.Buffer {
+	md := goldmark.New(
+          goldmark.WithExtensions(extension.GFM),
+          goldmark.WithParserOptions(
+              parser.WithAutoHeadingID(),
+							parser.WithParagraphTransformers(),
+          ),
+          goldmark.WithRendererOptions(
+              html.WithUnsafe(),
+							html.WithHardWraps(),
+          ),
+      )
+			
+	var buf bytes.Buffer
+
+	
+	if err := md.Convert([]byte(content), &buf); err != nil {
+		panic(err)
+	}
+	return buf
+}
+
+func openBrowser(url string) (err error) {
+	switch runtime.GOOS {
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		err = exec.Command("open", url).Start()
+	default:
+		err = fmt.Errorf("unsupported platform")
+	}
+
+	return err
 }
